ea9d0b5fa82421462da182d51253b9460da3e606
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-02-08 14:26:31 +0100

    ✅ Test RDP

---
+++ tests/testthat/test-RDP.R
@@ -0,0 +1,55 @@
+test_that("Small RDP example", {
+    expect_equal(
+        RDP(1:3, 1:3, 0.5),
+        data.frame(x = c(1, 3), y = c(1, 3))
+    )
+})
+
+
+test_that("Varying epsilon", {
+    x <- c(0, 1, 3, 5)
+    y <- c(2, 1, 0, 1)
+
+    expect_equal(
+        RDP(x, y, 0.5),
+        data.frame(x = c(0, 3, 5), y = c(2, 0, 1))
+    )
+
+    expect_equal(
+        RDP(x, y, 0.1),
+        data.frame(x = c(0, 1, 3, 5), y = c(2, 1, 0, 1))
+    )
+})
+
+
+test_that("Vertical segments", {
+    x <- c(10, 15, 15, 20)
+    y <- c(35, 34, 30, 29)
+
+    expect_equal(
+        RDP(x, y, 10),
+        data.frame(x = c(10, 20), y = c(35, 29))
+    )
+})
+
+
+test_that("Horizontal segments", {
+    x <- c(10, 15, 16, 21)
+    y <- c(35, 35, 30, 30)
+
+    expect_equal(
+        RDP(x, y, 1),
+        data.frame(x = c(10, 15, 16, 21), y = c(35, 35, 30, 30))
+    )
+})
+
+
+test_that("Bigger example", {
+    x <- c(3.5, 7.3, 23.2, 37.2, 54.6, 62.2, 71.5, 101.3)
+    y <- c(21.25, 12.0, 3.1, 12.07, 18.15, 16.45, 9.7, 21.1)
+
+    expect_equal(
+        RDP(x, y, 5),
+        data.frame(x = c(3.5, 23.2, 54.6, 71.5, 101.3), y = c(21.25, 3.1, 18.15, 9.7, 21.1))
+    )
+})
