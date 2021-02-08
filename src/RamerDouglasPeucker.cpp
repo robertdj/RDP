8ee3ea9c3a5449364cbf5451d146fba680649528
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-02-08 16:34:21 +0100

    More space in the code

--- src/RamerDouglasPeucker.cpp
+++ src/RamerDouglasPeucker.cpp
@@ -12,28 +12,29 @@ double PerpendicularDistance(const Point &pt, const Point &lineStart, const Poin
 	double dx = lineEnd.first - lineStart.first;
 	double dy = lineEnd.second - lineStart.second;
 
-	//Normalise
-	double mag = pow(pow(dx,2.0)+pow(dy,2.0),0.5);
-	if(mag > 0.0)
+	// Normalise
+	double mag = pow(pow(dx,2.0) + pow(dy,2.0), 0.5);
+	if (mag > 0.0)
 	{
-		dx /= mag; dy /= mag;
+		dx /= mag;
+	    dy /= mag;
 	}
 
 	double pvx = pt.first - lineStart.first;
 	double pvy = pt.second - lineStart.second;
 
-	//Get dot product (project pv onto normalized direction)
+	// Get dot product (project pv onto normalized direction)
 	double pvdot = dx * pvx + dy * pvy;
 
-	//Scale line direction vector
+	// Scale line direction vector
 	double dsx = pvdot * dx;
 	double dsy = pvdot * dy;
 
-	//Subtract this from pv
+	// Subtract this from pv
 	double ax = pvx - dsx;
 	double ay = pvy - dsy;
 
-	return pow(pow(ax,2.0)+pow(ay,2.0),0.5);
+	return pow(pow(ax,2.0) + pow(ay,2.0), 0.5);
 }
 
 void RamerDouglasPeucker(const vector<Point> &pointList, double epsilon, vector<Point> &out)
@@ -44,8 +45,8 @@ void RamerDouglasPeucker(const vector<Point> &pointList, double epsilon, vector<
 	// Find the point with the maximum distance from line between start and end
 	double dmax = 0.0;
 	size_t index = 0;
-	size_t end = pointList.size()-1;
-	for(size_t i = 1; i < end; i++)
+	size_t end = pointList.size() - 1;
+	for (std::size_t i = 1; i < end; i++)
 	{
 		double d = PerpendicularDistance(pointList[i], pointList[0], pointList[end]);
 		if (d > dmax)
@@ -56,25 +57,25 @@ void RamerDouglasPeucker(const vector<Point> &pointList, double epsilon, vector<
 	}
 
 	// If max distance is greater than epsilon, recursively simplify
-	if(dmax > epsilon)
+	if (dmax > epsilon)
 	{
 		// Recursive call
 		vector<Point> recResults1;
 		vector<Point> recResults2;
-		vector<Point> firstLine(pointList.begin(), pointList.begin()+index+1);
-		vector<Point> lastLine(pointList.begin()+index, pointList.end());
+		vector<Point> firstLine(pointList.begin(), pointList.begin() + index + 1);
+		vector<Point> lastLine(pointList.begin() + index, pointList.end());
 		RamerDouglasPeucker(firstLine, epsilon, recResults1);
 		RamerDouglasPeucker(lastLine, epsilon, recResults2);
 
 		// Build the result list
-		out.assign(recResults1.begin(), recResults1.end()-1);
+		out.assign(recResults1.begin(), recResults1.end() - 1);
 		out.insert(out.end(), recResults2.begin(), recResults2.end());
-		if(out.size()<2)
+		if (out.size() < 2)
 			throw runtime_error("Problem assembling output");
 	}
 	else
 	{
-		//Just return start and end points
+		// Just return start and end points
 		out.clear();
 		out.push_back(pointList[0]);
 		out.push_back(pointList[end]);
